ea0a1aef63293d0038baa52d25bf6e75c8b24a18
Park Joon-Kyu <kyu@theori.io>
2026-07-02 17:23:15 +0900

    Do not raise exception when empty sheet

--- df2md/__init__.py
+++ df2md/__init__.py
@@ -6,7 +6,7 @@ from .df2md import *
 
 
 def convert_xlsx(file_stream: BinaryIO) -> str:
-    sheets = pl.read_excel(file_stream, sheet_id=0, drop_empty_cols=False)
+    sheets = pl.read_excel(file_stream, sheet_id=0, drop_empty_cols=False, raise_if_empty=False)
     md = ""
     if isinstance(sheets, dict):
         for s in sheets:
