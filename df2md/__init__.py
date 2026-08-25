bd11dc0f0ca2d9578f9d956b0c0c276da571e109
Park Joon-Kyu <kyu@theori.io>
2026-08-25 10:33:43 +0900

    fallback to string

--- df2md/__init__.py
+++ df2md/__init__.py
@@ -6,7 +6,7 @@ from .df2md import *
 
 
 def convert_xlsx(file_stream: BinaryIO) -> str:
-    sheets = pl.read_excel(file_stream, sheet_id=0, drop_empty_cols=False, raise_if_empty=False)
+    sheets = pl.read_excel(file_stream, sheet_id=0, drop_empty_cols=False, raise_if_empty=False, infer_schema_length=0)
     md = ""
     if isinstance(sheets, dict):
         for s in sheets:
